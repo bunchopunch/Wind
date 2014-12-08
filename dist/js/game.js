d86509b0844af469bb416aa9705c06a9d481e60b
bunchopunch <bunchopunch@mustacheforpresident.com>
2014-12-07 21:16:37 -0700

    Adding improved collisions and handelers

--- dist/js/game.js
+++ dist/js/game.js
@@ -104,8 +104,10 @@ module.exports = Menu;
   function Play() {}
   Play.prototype = {
     playerCursorKeys: null,
-    levelObjects: null,
+    starLayer: null,
+    goalLayer: null,
     player: null,
+    goal: null,
     create: function() {
       var game = this.game;
       var levels = [
@@ -114,45 +116,53 @@ module.exports = Menu;
           [12, 8, 3, 600],
           [15, 11, 4, 800]
         ]
-      var currentLevel = 1;
+      var currentLevel = 4;
 
+      // Init keyboard
       this.playerCursorKeys = game.input.keyboard.createCursorKeys();
-      // Set up physics
+
+      // Init game physics
       game.physics.startSystem(Phaser.Physics.ARCADE);
 
-      this.levelObjects = this.game.add.group();
-      this.levelObjects.x = 500;
-      this.levelObjects.y = 350;
+      // Init group for stars
+      this.starLayer = this.game.add.group();
+      this.starLayer.x = 500;
+      this.starLayer.y = 350;
+
+      // Init group for stars
+      this.goalLayer = this.game.add.group();
+      this.goalLayer.x = 500;
+      this.goalLayer.y = 350;
 
       // Create the stars
       for (var i = 0; i < levels[currentLevel - 1][1 - 1]; i++) {
-        this.createStar(game.world.randomX, this.randomPostion('y'), 'small');
+        this.starLayer.add(this.createStar(game.world.randomX, this.randomPostion('y'), 'small'));
       };
       for (var i = 0; i < levels[currentLevel - 1][2 - 1]; i++) {
-        this.createStar(this.randomPostion('x'), this.randomPostion('y'), 'medium');
+        this.starLayer.add(this.createStar(this.randomPostion('x'), this.randomPostion('y'), 'medium'));
       };
       for (var i = 0; i < levels[currentLevel - 1][3 - 1]; i++) {
-        this.createStar(this.randomPostion('x'), this.randomPostion('y'), 'large');
+        this.starLayer.add(this.createStar(this.randomPostion('x'), this.randomPostion('y'), 'large'));
       };
 
       // Create a goal object
-      this.createGoal(levels[currentLevel - 1][3]);
+      this.goalLayer.add(this.goal = this.createGoal(levels[currentLevel - 1][3]));
 
       // Create the player ship
-      this.createPlayer( this.game.world.centerX, this.game.world.centerY );
-
-//      this.sprite.events.onInputDown.add(this.clickListener, this);
-
+      this.player = this.createPlayer( this.game.world.centerX, this.game.world.centerY );
     },
 
-    collisionH: function(){
+    deathHandler: function(){
+      console.log("Deaded")
+    },
 
- //     alert('congratz, you!');
-
+    winHandler: function(){
+      console.log("A winner is you!");
     },
 
     update: function() {
-      this.game.physics.arcade.collide(this.player, this.levelObjects, this.collisionH());
+      this.game.physics.arcade.collide(this.player, this.starLayer, this.deathHandler, null, this);
+      this.game.physics.arcade.collide(this.player, this.goal, this.winHandler, null, this);
 
       if (this.playerCursorKeys.up.isDown) {
         this.game.physics.arcade.accelerationFromRotation(this.player.rotation, 200, this.player.body.acceleration);
@@ -163,7 +173,6 @@ module.exports = Menu;
       if (this.playerCursorKeys.left.isDown) {
           this.player.body.angularVelocity = -200;
       } else if (this.playerCursorKeys.right.isDown) {
-//        this.player.rotation += 0.05;
           this.player.body.angularVelocity = 200;
       } else {
         this.player.body.angularVelocity = 0;
@@ -172,10 +181,10 @@ module.exports = Menu;
       this.game.camera.focusOnXY(this.player.x, this.player.y + this.player.height - this.camera.view.halfHeight);
 
     },
-    // HELPER THINGS
+
     randomInRange: function(min, max){
       var returning = Math.floor(Math.random() * (min - max)) + min;
-      return returning; // PLUSSSSS MIIIIIINNNNNN AT THE ENNNNNNNNDDDDDDDDDDDDD. I'm
+      return returning; 
     },
 
     randomPostion: function(axis){
@@ -185,27 +194,6 @@ module.exports = Menu;
         var hay = this.game.world.randomY;
         return hay - this.game.world.height/2
       }
-    },
-
-    createStar: function(newStarX, newStarY, newStarSize){
-//        this.levelObjects.add(this.game.add.sprite(newStarX, newStarY, 'star64') );
-      var starImage = 'star14';
-      if(newStarSize === 'small') {
-        starImage = 'star14';
-      } else if (newStarSize === 'medium') {
-        starImage = 'star32';
-      } else if (newStarSize === 'large') {
-        starImage = 'star64';
-      }
-      var newStar = this.game.add.sprite(newStarX, newStarY, starImage)
-      this.game.physics.enable(newStar, Phaser.Physics.ARCADE);
-
-      newStar.body.enable;
-
-//      this.game.physics.arcade.velocityFromAngle(this.levelObjects.angle, 300)
-//      newStar.body.velocity.x = 200;
-      this.levelObjects.add(newStar);
-
     },
 
     generateDistantX: function(minDistance){
@@ -228,24 +216,50 @@ module.exports = Menu;
       }
     },
 
+    createStar: function(newStarX, newStarY, newStarSize){
+      var starImage = 'star14';
+      if(newStarSize === 'small') {
+        starImage = 'star14';
+      } else if (newStarSize === 'medium') {
+        starImage = 'star32';
+      } else if (newStarSize === 'large') {
+        starImage = 'star64';
+      }
+
+      var newStar = this.game.add.sprite(newStarX, newStarY, starImage);
+      this.game.physics.enable(newStar, Phaser.Physics.ARCADE);
+      newStar.body.enable;
+      newStar.body.immovable = true;
+
+      return newStar;
+    },
+
     createGoal: function(minDistance){
-//        minDistance = minDistance/2;
-//        var goal = this.game.add.sprite(this.generateDistantX(minDistance), this.generateDistantY(minDistance), 'goal');
-      var goal = this.game.add.sprite(this.randomPostion('x'), this.randomPostion('y'), 'goal');
-      this.game.physics.enable(goal, Phaser.Physics.ARCADE);
-      this.levelObjects.add(goal);
+      var newGoal = this.game.add.sprite(this.randomPostion('x'), this.randomPostion('y'), 'goal');
+      this.game.physics.enable(newGoal, Phaser.Physics.ARCADE);
+
+      newGoal.body.enable;
+      newGoal.body.immovable = true;
+
+      return newGoal;
     },
 
     createPlayer: function(newPlayerX, newPlayerY){
-      this.player = this.game.add.sprite(newPlayerX, newPlayerY, 'ship');
-      this.player.anchor.set(0.5);
-      this.player.angle = 1.5 * Math.PI;
-      this.player.rotation = 0;
-      this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+      var newPlayer = this.game.add.sprite(newPlayerX, newPlayerY, 'ship');
+      this.game.physics.enable(newPlayer, Phaser.Physics.ARCADE);
+
+      newPlayer.body.enable;
+      newPlayer.anchor.set(0.5);
+      newPlayer.angle = 1.5 * Math.PI;
+      newPlayer.rotation = 0;
+
+      return newPlayer;
     },
-    clickListener: function() {
-      this.game.state.start('gameover');
-    }
+
+    // OLD
+//    clickListener: function() {
+//      this.game.state.start('gameover');
+//    }
   };
 
   module.exports = Play;
